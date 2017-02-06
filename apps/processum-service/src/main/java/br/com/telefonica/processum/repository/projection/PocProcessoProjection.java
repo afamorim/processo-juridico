cc4b69497acbf3f5a9dd0282977de970b9321195
Joel Amálio dos Santos Filho <joel.amalio@gmail.com>
2017-02-06 14:51:40 -0300

    Configuração do timestamp.

--- apps/processum-service/src/main/java/br/com/telefonica/processum/repository/projection/PocProcessoProjection.java
+++ apps/processum-service/src/main/java/br/com/telefonica/processum/repository/projection/PocProcessoProjection.java
@@ -1,50 +1,97 @@
 package br.com.telefonica.processum.repository.projection;
 
-import br.com.telefonica.processum.model.PocOcorrencia;
+import br.com.telefonica.processum.model.PocProcesso;
 import org.springframework.data.rest.core.config.Projection;
-
 
 /**
  *
  * @author jadossantos
  */
-@Projection(name = "projecao", types = PocOcorrencia.class)
-public interface PocOcorrenciaProjection {
-    
-    String getIdPocOcorrencia();
-    
-    String getStsPocOcorrencia();
-    
-    String getDtcPocOcorrencia();
-    
-    String getNumProcesso();
-    
-    String getNumCpfCnpj();
-    
-    String getNumLinha();
-    
-    String getDtcInicioPequisa();
-    
-    String getDtcFimPesquisa();
-    
-    String getNomCliente();
-    
-    String getNomConta();
+@Projection(name = "projecao", types = PocProcesso.class)
+public interface PocProcessoProjection {
 
-    String getDtcCadastro();
-    
-    String getNomTipoConta();
-    
-    String getNomTipoAssinatura();
-    
-    String getStsAssinatura();
-    
-    String getDesEnderecoCorrespondencia();
-    
-    String getDesEnderecoTributacao();
-    
-    String getValSaldo();
-    
-    String getNomFormaPagamento();
-    
+    String getCodEspecificacaoNatureza();
+
+    String getDesEspecificacaoNatureza();
+
+    String getCodDetalheEspecificNatureza();
+
+    String getNomDetalheEspecificNatureza();
+
+    String getCodPreObjetoEmpresa();
+
+    String getNomPreObjetoEmpresa();
+
+    String getDtcProtocoloAforamento();
+
+    String getStsPassouCallCenter();
+
+    String getStsConsumerista();
+
+    String getStsPassouProcon();
+
+    String getNomTipoAcaoProcesso();
+
+    String getCodDivisaoProcesso();
+
+    String getDtcEnvioEscContratado();
+
+    String getCodTipoAcaoProcesso();
+
+    String getSigEscritorioContratado();
+
+    String getNomEscritorioContratado();
+
+    String getSigCorresponsavel();
+
+    String getNomCorresponsavel();
+
+    String getNomPoloPassivo();
+
+    String getNomTerceiroInteressado();
+
+    String getIdPocProcesso();
+
+    String getCodEmpresa();
+
+    String getNomEmpresa();
+
+    String getCodNatureza();
+
+    String getDesNatureza();
+
+    String getDtcCitacao();
+
+    String getStsRelevante();
+
+    String getNomDivisao();
+
+    String getNumProcesso();
+
+    String getCodTipoAcao();
+
+    String getDesTipoAcao();
+
+    String getSigEstado();
+
+    String getNomEstado();
+
+    String getSigMunicipio();
+
+    String getNomMunicipio();
+
+    String getCodComarca();
+
+    String getNomComarca();
+
+    String getCodJuizo();
+
+    String getNomJuizo();
+
+    String getSigAdvogado();
+
+    String getNomAdvogado();
+
+    String getNomPoloAtivo();
+
 }
